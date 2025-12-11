f7af129678dd010d4b5f2df6f8efaf1623fcab56
mlo <roukell@hotmail.com>
2025-12-11 21:41:53 +1100

    add ubuntu instance

--- data.tf
+++ data.tf
@@ -2,3 +2,7 @@
 data "aws_ssm_parameter" "al2023_ami" {
   name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
 }
+
+data "aws_ssm_parameter" "ubuntu_24_04_ami" {
+  name = "/aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id"
+}
--- main.tf
+++ main.tf
@@ -1,36 +1,68 @@
-resource "aws_instance" "example" {
-  ami                    = data.aws_ssm_parameter.al2023_ami.value
-  instance_type          = "t2.micro" # free tier eligible
+# resource "aws_instance" "amazon_linux_2023" {
+#   ami                    = data.aws_ssm_parameter.al2023_ami.value
+#   instance_type          = "t3.medium" # free tier eligible
+#   iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
+#   vpc_security_group_ids = [aws_security_group.ssm_and_local_only.id]
+
+#   # Install Docker Engine on Amazon Linux 2023
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               set -xe
+
+#               # Update system
+#               dnf update -y
+
+#               # Install Docker
+#               dnf install -y docker git vim
+
+#               # Enable + start Docker
+#               systemctl enable docker
+#               systemctl start docker
+
+#               # Add ec2-user to docker group
+#               usermod -aG docker ec2-user
+
+#               # Apply group changes WITHOUT requiring logout
+#               # This runs in a safe subshell to avoid breaking cloud-init
+#               su - ec2-user -c "newgrp docker << 'EONG'
+#               exit
+#               EONG"
+#               EOF
+
+#   tags = {
+#     Name = "amazon_linux_2023"
+#   }
+# }
+
+resource "aws_instance" "ubuntu" {
+  ami                    = data.aws_ssm_parameter.ubuntu_24_04_ami.value
+  instance_type          = "t3.medium"
   iam_instance_profile   = aws_iam_instance_profile.ssm_profile.name
   vpc_security_group_ids = [aws_security_group.ssm_and_local_only.id]
 
-  # Install Docker Engine on Amazon Linux 2023
+  # Install Docker Engine on Ubuntu 24.04
   user_data = <<-EOF
-              #!/bin/bash
-              set -xe
-
-              # Update system
-              dnf update -y
-
-              # Install Docker
-              dnf install -y docker git vim
-
-              # Enable + start Docker
-              systemctl enable docker
-              systemctl start docker
-
-              # Add ec2-user to docker group
-              usermod -aG docker ec2-user
-
-              # Apply group changes WITHOUT requiring logout
-              # This runs in a safe subshell to avoid breaking cloud-init
-              su - ec2-user -c "newgrp docker << 'EONG'
-              exit
-              EONG"
-              EOF
+  #!/bin/bash
+  set -xe
+  
+  # Update system
+  apt-get update
+  
+  # Install Docker
+  apt-get install -y docker.io git vim bridge-utils
+  
+  # Enable + start Docker
+  systemctl enable docker
+  systemctl start docker
+  
+  # Add ubuntu user to docker group
+  usermod -aG docker ubuntu
+  
+  # Group changes will take effect on next login; no need to run newgrp in cloud-init
+  EOF
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "ubuntu"
   }
 }
 
