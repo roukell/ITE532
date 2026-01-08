0b23c1d2c4c1f1bd09ce2243d44a4cb815d9a8b4
mlo <roukell@hotmail.com>
2026-01-09 01:23:10 +1100

    update user data

--- main.tf
+++ main.tf
@@ -49,12 +49,25 @@ resource "aws_instance" "ubuntu" {
   apt-get update
   
   # Install Docker
-  apt-get install -y docker.io git vim bridge-utils amazon-ssm-agent docker-compose-plugin
-  
+  apt-get install -y docker.io git vim bridge-utils amazon-ssm-agent ca-certificates curl gnupg
+
   # Enable + start Docker
   systemctl enable docker
   systemctl start docker
   systemctl enable amazon-ssm-agent
+
+  sudo install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+  echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  apt-get update
+  apt-get install docker-compose-plugin
+
+  docker compose version
   
   # Add ubuntu user to docker group
   usermod -aG docker ubuntu
